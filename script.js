b0fbd37ac5a64d704bd3343a2d8ec5c342ca6afd
Mohammad Adnan <65945728+adnandotdev@users.noreply.github.com>
2025-02-08 18:12:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+fName = document.getElementById('f-name')
+lName = document.getElementById('l-name')
+number = document.getElementById('number')
+email = document.getElementById('email')
+document.getElementById('submit').addEventListener('click', submit)
+function submit(){
+	alert(`First Name: ${fName.value} Last Name: ${lName.value} Phone Number: ${number.value} Email ID: ${email.value}`)
+}
